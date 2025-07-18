e66445e509fca6edf9fd6b6b1133c289d812d9c7
aurora74 <quockhanh.hsgs@gmail.com>
2025-07-18 15:14:09 +0700

    done chapter 3, 4, 5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
+  private
+
+  def set_locale
+    if Settings.i18n.available_locales.include?(params[:locale])
+      locale = params[:locale]
+    end
+    I18n.locale = locale || Settings.i18n.default_locale
+  end
+
+  def default_url_options
+    {locale: I18n.locale}
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def full_title page_title = ""
+    base_title = Settings.app.name
+    page_title.empty? ? base_title : "#{page_title} | #{base_title}"
+  end
+
+  def link_to_locale locale_key
+    locale_name = locale_key.to_s.upcase
+    current_locale = I18n.locale.to_s
+    css_class = current_locale == locale_key.to_s ? "active" : ""
+
+    link_to locale_name, url_for(locale: locale_key), class: css_class
+  end
 end
