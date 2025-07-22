9f1ab2eeb36971e4e422ff1db980000dbe42b9d2
aurora74 <quockhanh.hsgs@gmail.com>
2025-07-23 06:47:17 +0700

    done chapter 7

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
     root "microposts#index"
+    
     resources :microposts, only: [:index]
+    resources :users, only: [:show]
+    
     get "/help", to: "static_pages#help"
     get "/about", to: "static_pages#about"
     get "/contact", to: "static_pages#contact"
+    get "/signup", to: "users#new"
+    post "/signup", to: "users#create"
   end
 end
